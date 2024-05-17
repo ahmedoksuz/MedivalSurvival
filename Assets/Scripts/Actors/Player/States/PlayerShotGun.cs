b33d73e56ee1717942c64e7173fe40d84518e4f8
ahmedoksuz <34533185+ahmedoksuz@users.noreply.github.com>
2024-05-17 17:53:27 +0300

    Save Sve Sve Alahın Belası

---
+++ Assets/Scripts/Actors/Player/States/PlayerShotGun.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PufferSoftware.Scripts.Player.StateMachine
+{
+    public class PlayerShotGun : PlayerBaseState
+    {
+        public PlayerShotGun(PlayerStateMachine stateMachine) : base(stateMachine)
+        {
+            
+        }
+    }
+}
